229475768d7fcfbfd7bd2ab25b5790e24b412c65
Peggy <peggicl861217@gmail.com>
2025-03-04 09:29:51 +0800

    0304

--- src/pages/theme.vue
+++ src/pages/theme.vue
@@ -1,23 +1,9 @@
-<!-- <template>
-  <v-card color="grey-lighten-4" height="200px" rounded="0" flat>
-    <v-toolbar extended prominent>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+<template>
+  <div>Theme Page</div>
+</template>
 
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
-</template> -->
+<script>
+export default {
+  name: 'ThemePage',
+}
+</script>
