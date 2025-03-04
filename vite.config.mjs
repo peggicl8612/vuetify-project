919ff7fa0e8e8f0726d86b2805636e45ec4de6f2
Peggy <peggicl861217@gmail.com>
2025-03-04 13:28:43 +0800

    0304

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vuetify-project/',
   plugins: [
     VueRouter(),
     Layouts(),
