69c1c6da22af5aeec192d1e47ad07fbe57a8e425
peggicl8612 <peggicl861217@gmail.com>
2025-03-05 04:23:36 +0000

    Deploying to gh-pages from @ peggicl8612/vuetify-project@ffcb87fd803a93e6df81d265cf8ae2fa7cb8bd59 🚀

--- assets/adminLayout-ZeyOlWdf.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,r as t,o as a,h as s,E as o,d as l}from"./index-DPqlCyAx.js";const d={};function u(i,e){const n=t("router-view");return a(),s("div",null,[e[0]||(e[0]=o("header",null,"Admin Layout",-1)),o("main",null,[l(n)]),e[1]||(e[1]=o("footer",null,"Admin Footer",-1))])}const c=r(d,[["render",u]]);export{c as default};
